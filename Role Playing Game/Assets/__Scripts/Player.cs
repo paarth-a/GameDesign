aaf69d221d033e0b04d273409e06a7d484d9b2f2
mjohn386 <mjohn386@uwo.ca>
2020-03-15 17:40:06 -0400

    Design level 1 & added parent scripts

---
+++ Role Playing Game/Assets/__Scripts/Player.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    static public Player S;
+
+    public float speed = 10f;
+    public float health = 10f;
+    public float defence = 10f;
+    public float attack = 10f;
+    public float ranged = 10f;
+
+    public Vector3 pos
+    {
+        get
+        {
+            return (this.transform.position);
+        }
+        set
+        {
+            this.transform.position = value;
+        }
+    }
+
+    void Awake()
+    {
+        if (S == null)
+        {
+            S = this;
+        }
+        else
+        {
+            Debug.LogError("Player.Awake() - Attempted to assign second Player.S!");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+    }
+}
