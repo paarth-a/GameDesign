c9b0e63d36d9aecd32c5d4eea890bc77127b166b
mjohn386 <mjohn386@uwo.ca>
2020-03-18 22:09:05 -0400

    Fixed Robert's Mess

---
+++ Role Playing Game/Assets/__Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public float speed = 10f;
+    public float health = 10f;
+    public float defence = 10f;
+    public float attack = 10f;
+    public float ranged = 10f;
+
+    public Vector3 pos
+    {
+        get
+        {
+            return (this.transform.position);
+        }
+        set
+        {
+            this.transform.position = value;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
