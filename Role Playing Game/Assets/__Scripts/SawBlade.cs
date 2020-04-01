92f2ae07740b19766cdf80682f5f44b978ef05c5
mjohn386 <mjohn386@uwo.ca>
2020-04-01 18:23:12 -0400

    added new enemies

---
+++ Role Playing Game/Assets/__Scripts/SawBlade.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawBlade : Enemy
+{
+    public override void Attack()
+    {
+        float step = 1f * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, Player.S.transform.position, step);
+    }
+}
